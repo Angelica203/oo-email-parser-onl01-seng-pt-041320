3635236749fbec25b426147c8a39b8e60a8d7eb8
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-10 18:26:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
   end
 
  def parse
-   cvs_emails.spilt. collect do |address| address.split(',')
+   cvs_emails.split. collect do |address| address.split(',')
    end
    .flatten.uniq
    end
